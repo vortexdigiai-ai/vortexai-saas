d6de4031b3ccbe4e7c2ab8e6f78dd3d12ffe2911
LeCraM-H <marcelmoly1@gmail.com>
2026-08-30 00:56:09 +0200

    Widget solucionado

--- public/widget.js
+++ public/widget.js
@@ -1,5 +1,6 @@
 (function () {
   var script = document.currentScript;
+
   var tiendaId = script.getAttribute('data-tienda-id');
 
   if (!tiendaId) {
@@ -10,16 +11,31 @@
   var baseUrl = script.src.replace('/widget.js', '');
 
   var iframe = document.createElement('iframe');
-  iframe.src = baseUrl + '/widget/' + tiendaId;
+
+  iframe.src = baseUrl + '/widget/' + encodeURIComponent(tiendaId);
+
   iframe.style.position = 'fixed';
-  iframe.style.bottom = '0';
-  iframe.style.right = '0';
-  iframe.style.width = '400px';
-  iframe.style.height = '600px';
+  iframe.style.top = '0';
+  iframe.style.left = '0';
+  iframe.style.width = '100vw';
+  iframe.style.height = '100vh';
+
   iframe.style.border = 'none';
+  iframe.style.margin = '0';
+  iframe.style.padding = '0';
+
   iframe.style.zIndex = '999999';
+
   iframe.style.background = 'transparent';
+
+  iframe.style.pointerEvents = 'none';
+
   iframe.setAttribute('allowtransparency', 'true');
+
+  iframe.setAttribute(
+    'title',
+    'VortexAI Chatbot'
+  );
 
   document.body.appendChild(iframe);
 })();
